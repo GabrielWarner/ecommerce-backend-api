9bf8d2fee1e75d85f68980e78f15251d4703de2b
GabrielWarner <gabe0412@uw.edu>
2022-07-31 15:47:21 -0700

    fetch categorie based on ID and also returns the products within the category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,32 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  await Category.findAll({
+    include:[Product]
+}).then(data=>{
+    res.json(data)
+}).catch(err=>{
+    res.status(500).json({msg:"oh noes! error!",err})
+})
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  id = req.params.id
+await Category.findOne({
+  where:{
+    id: req.params.id,
+  },
+  include:[Product]
+}).then(data=>{
+  res.json(data)
+}).catch(err=>{
+  res.status(500).json({msg:"oh noes! error!",err})
+})
 });
 
 router.post('/', (req, res) => {
