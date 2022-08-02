d094d325081bdb1b3bc8cad2021eef6afeade7e7
GabrielWarner <gabe0412@uw.edu>
2022-08-02 09:57:51 -0700

    functionality to delete tag

--- models/index.js
+++ models/index.js
@@ -12,11 +12,20 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: 'cascade',
 });
-
 Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'cascade',
 });
+
+Tag.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'cascade',
+});
+Category.hasMany(Tag, {
+  foreignKey: 'category_id',
+  onDelete: 'cascade',
+});
+
 Product.belongsToMany(Tag, {through: ProductTag,foreignKey: 'product_id'})
 Tag.belongsToMany(Product, {through: ProductTag, foreignKey: 'tag_id'})
 
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,21 +1,61 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models');
 
 // The `/api/tags` endpoint
 
 router.get('/', async(req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-  
+  Tag.findAll({
+    include: [
+      Category,
+      {
+        model: Product,
+        through:[ProductTag],
+      }]
+}).then(data=>{
+    res.json(data)
+}).catch(err=>{
+    res.status(500).json({msg:"oh noes! error!",err})
+})
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  id = req.params.id
+  Tag.findOne({
+    where:{
+      id: req.params.id,
+    },
+    include:[Product]
+  }).then(data=>{
+    res.json(data)
+  }).catch(err=>{
+    res.status(500).json({msg:"oh noes! error!",err})
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((tag) => {
+    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+    if (req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      return ProductTag.bulkCreate(productTagIdArr);
+    }
+    // if no product tags, just respond
+    res.status(200).json(product);
+  })
+  .then((productTagIds) => res.status(200).json(productTagIds))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
@@ -24,6 +64,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+        id:req.params.id
+    }
+    }).then(tag=>{
+        if(!tag){
+            return res.status(404).json({msg:"no such tag!"})
+        }
+    res.json(tag)
+}).catch(err=>{
+    res.status(500).json({
+        msg:"internal server error",
+        err
+    })
+})
 });
 
 module.exports = router;
